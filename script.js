5d90c6be6c17d135405bc64ab42dd1b018d0bc12
shantiox <danaciocoiu78@gmail.com>
2019-01-18 21:58:50 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
-
+var model = {
+	boardSize: 7,
+	numShips: 3,
+	shipLength: 3,
+	shipsSunk: 0,
+	
+	ships: [
+		{ locations: [0, 0, 0], hits: ["", "", ""] },
+		{ locations: [0, 0, 0], hits: ["", "", ""] },
+		{ locations: [0, 0, 0], hits: ["", "", ""] }
+	],
 
 var view = {
 
@@ -26,3 +36,4 @@ view.displayHit("12");
 view.displayMiss("25");
 view.displayHit("26");
 view.displayMessage("Tap tap, is this thing on?");
+
